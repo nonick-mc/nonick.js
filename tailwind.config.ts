9f89cc44efdb2d9e169f889bb9dcfb8e0e67b284
nonick <87268971+nonick-mc@users.noreply.github.com>
2024-10-16 01:48:22 +0900

    feat: AutoMod Plusの設定ページを追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(avatar|button|card|chip|code|divider|dropdown|input|link|listbox|modal|navbar|radio|scroll-shadow|select|skeleton|snippet|spinner|toggle|user|ripple|menu|popover).js"
+    "./node_modules/@nextui-org/theme/dist/components/(accordion|avatar|button|card|chip|code|divider|dropdown|input|link|listbox|modal|navbar|radio|scroll-shadow|select|skeleton|snippet|spinner|toggle|user|ripple|menu|popover).js"
   ],
   theme: {
     container: {
