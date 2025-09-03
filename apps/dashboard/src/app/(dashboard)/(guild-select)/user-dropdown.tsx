857227fee57b616315bb4d1591c833b2a04ec4b1
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-09-03 15:54:16 +0000

    feat(dashboard): user-dropdownのメニュー部分を別コンポーネントに切り出し

--- apps/dashboard/src/app/(dashboard)/(guild-select)/user-dropdown.tsx
+++ apps/dashboard/src/app/(dashboard)/(guild-select)/user-dropdown.tsx
@@ -1,30 +1,16 @@
 ﻿'use client';
 
-import { LayoutDashboardIcon, LogInIcon, PaintbrushIcon } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { useTheme } from 'next-themes';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { Skeleton } from '@/components/ui/skeleton';
+import { UserDropdownMenuContent } from '@/components/user-dropdown-menu';
 import { authClient } from '@/lib/auth-client';
 
 export function UserDropdown() {
   const { data: session } = authClient.useSession();
-  const { theme, setTheme } = useTheme();
-  const router = useRouter();
 
   return (
     <DropdownMenu>
@@ -37,61 +23,7 @@ export function UserDropdown() {
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end' className='w-56'>
-        <DropdownMenuLabel>
-          {session ? (
-            <div className='flex gap-2 items-center'>
-              <Avatar className='size-8'>
-                <AvatarImage src={session?.user.image ?? undefined} alt={`@${session.user.name}`} />
-                <AvatarFallback>{session.user.globalName ?? session.user.name}</AvatarFallback>
-              </Avatar>
-              <section className='leading-tight'>
-                <p className='text-foreground text-sm'>
-                  {session.user.globalName ?? session.user.name}
-                </p>
-                <p className='text-zinc-500 text-xs'>@{session.user.name}</p>
-              </section>
-            </div>
-          ) : (
-            <Skeleton className='h-8' />
-          )}
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem onClick={() => router.push('/')}>
-            <LayoutDashboardIcon />
-            ダッシュボード
-          </DropdownMenuItem>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger className='flex gap-2 [&_svg]:size-4'>
-              <PaintbrushIcon className='stroke-muted-foreground' />
-              テーマ
-            </DropdownMenuSubTrigger>
-            <DropdownMenuSubContent>
-              <DropdownMenuRadioGroup value={theme} onValueChange={(v) => setTheme(v)}>
-                <DropdownMenuRadioItem value='system'>システム</DropdownMenuRadioItem>
-                <DropdownMenuRadioItem value='light'>ライト</DropdownMenuRadioItem>
-                <DropdownMenuRadioItem value='dark'>ダーク</DropdownMenuRadioItem>
-              </DropdownMenuRadioGroup>
-            </DropdownMenuSubContent>
-          </DropdownMenuSub>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem
-            onClick={async () =>
-              await authClient.signOut({
-                fetchOptions: {
-                  onSuccess: () => router.refresh(),
-                },
-              })
-            }
-            variant='destructive'
-            disabled={!session}
-          >
-            <LogInIcon />
-            ログアウト
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
+        <UserDropdownMenuContent />
       </DropdownMenuContent>
     </DropdownMenu>
   );
