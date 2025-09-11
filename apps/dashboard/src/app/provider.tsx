b0e74286420142df7e760b8182a936ee6b294177
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-09-11 07:18:09 +0000

    feat(dashboard): ルートlayout.tsxのセットアップ

---
+++ apps/dashboard/src/app/provider.tsx
@@ -0,0 +1,14 @@
+﻿'use client';
+
+import { ThemeProvider } from 'next-themes';
+import NextTopLoader from 'nextjs-toploader';
+import type { PropsWithChildren } from 'react';
+
+export function Provider({ children }: PropsWithChildren) {
+  return (
+    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
+      <NextTopLoader color='#155dfc' showSpinner={false} />
+      {children}
+    </ThemeProvider>
+  );
+}
