e8d407070d9c5c58328e5bb74978f6d0538b3462
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-09-12 18:07:28 +0000

    feat(dashboard): better-authの導入

---
+++ apps/dashboard/src/lib/auth.ts
@@ -0,0 +1,38 @@
+﻿import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from './drizzle';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+  }),
+  user: {
+    additionalFields: {
+      globalName: {
+        type: 'string',
+        required: false,
+        defaultValue: null,
+      },
+      discordUserId: {
+        type: 'string',
+        required: true,
+        unique: true,
+      },
+    },
+  },
+  socialProviders: {
+    discord: {
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+      scope: ['guilds'],
+      overrideUserInfoOnSignIn: true,
+      mapProfileToUser: (profile) => {
+        return {
+          name: profile.username,
+          globalName: profile.global_name,
+          discordUserId: profile.id,
+        };
+      },
+    },
+  },
+});
