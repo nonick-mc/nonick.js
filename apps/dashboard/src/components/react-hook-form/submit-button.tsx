f24ccf452d94ebc63bbefe213fbe43159cd586a7
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-09-13 14:18:32 +0000

    feat(dashboard): FormSubmitButtonコンポーネントを追加し、イベントログの各フォームにDividerとFormSubmitButtonを追加

---
+++ apps/dashboard/src/components/react-hook-form/submit-button.tsx
@@ -0,0 +1,19 @@
+﻿import { Button } from '@heroui/react';
+import { useFormState } from 'react-hook-form';
+import { Icon } from '../icon';
+
+export function FormSubmitButton() {
+  const { isSubmitting, isDirty } = useFormState();
+
+  return (
+    <Button
+      type='submit'
+      color='primary'
+      isLoading={isSubmitting}
+      isDisabled={!isDirty}
+      startContent={!isSubmitting && <Icon icon='solar:diskette-bold' className='text-2xl' />}
+    >
+      変更を保存
+    </Button>
+  );
+}
