4649faf443e3d666e780d83afaeb89d8d74b10f8
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-01 06:52:21 +0000

    feat(dashboard): next-themesの導入、ダークテーマの実装

---
+++ apps/dashboard/src/app/provider.tsx
@@ -0,0 +1,12 @@
+﻿'use client';
+
+import { ThemeProvider } from 'next-themes';
+import type { PropsWithChildren } from 'react';
+
+export function Provider({ children }: PropsWithChildren) {
+  return (
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+}
