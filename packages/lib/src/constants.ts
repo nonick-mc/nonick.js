f66f9126006b811ca1bec351b44dc52339705815
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-06 14:25:31 +0000

    Revert "feat(lib): 共通ライブラリパッケージを作成"
    This reverts commit 4d100a6dfd8fe9b560083e762c3d410e8958503c.

--- packages/lib/src/constants.ts
+++
@@ -1,6 +0,0 @@
-﻿export const Links = {
-  docs: 'https://docs.nonick-js.com',
-  homepage: 'https://nonick-js.com',
-  dashboard: 'https://dashboard.nonick-js.com',
-  supportServer: 'https://discord.gg/q4FcBm2P42',
-} as const;
