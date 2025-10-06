4d100a6dfd8fe9b560083e762c3d410e8958503c
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-06 13:23:05 +0000

    feat(lib): 共通ライブラリパッケージを作成

---
+++ packages/lib/src/constants.ts
@@ -0,0 +1,6 @@
+﻿export const Links = {
+  docs: 'https://docs.nonick-js.com',
+  homepage: 'https://nonick-js.com',
+  dashboard: 'https://dashboard.nonick-js.com',
+  supportServer: 'https://discord.gg/q4FcBm2P42',
+} as const;
