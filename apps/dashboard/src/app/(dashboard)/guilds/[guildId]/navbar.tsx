93ff11c99aaa25d56a1b76bca603b7997f2aa2d6
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-07 06:31:09 +0000

    style(dasboard): サイドバーのvariantを`inset`から`default`に変更

--- apps/dashboard/src/app/(dashboard)/guilds/[guildId]/navbar.tsx
+++ apps/dashboard/src/app/(dashboard)/guilds/[guildId]/navbar.tsx
@@ -3,7 +3,7 @@ import { UserDropdown } from '@/components/user-dropdown';
 
 export function Navbar() {
   return (
-    <nav className='sticky top-0 z-10 px-6 flex items-center justify-between h-16 bg-background border-b'>
+    <nav className='sticky top-0 z-10 px-6 flex items-center justify-between h-16 border-b bg-background'>
       <SidebarTrigger />
       <UserDropdown />
     </nav>
