b0d8c9cbec748dc80beb987f36274f5fc417e4e1
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-07 05:05:05 +0000

    feat(dashboard): sonnerを追加

--- apps/dashboard/src/app/provider.tsx
+++ apps/dashboard/src/app/provider.tsx
@@ -3,11 +3,13 @@
 import { ThemeProvider } from 'next-themes';
 import NextTopLoader from 'nextjs-toploader';
 import type { PropsWithChildren } from 'react';
+import { Toaster } from '@/components/ui/sonner';
 
 export function Provider({ children }: PropsWithChildren) {
   return (
     <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
       <NextTopLoader color='#2563eb' showSpinner={false} />
+      <Toaster />
       {children}
     </ThemeProvider>
   );
