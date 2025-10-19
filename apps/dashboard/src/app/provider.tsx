10ca7e8a6ccc59d7eb6648e6c8c78bad4fd36849
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-19 18:59:41 +0900

    style(dashboard): Toasterの表示位置を`top-center`に変更

--- apps/dashboard/src/app/provider.tsx
+++ apps/dashboard/src/app/provider.tsx
@@ -9,7 +9,14 @@ export function Provider({ children }: PropsWithChildren) {
   return (
     <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
       <NextTopLoader color='#2563eb' showSpinner={false} />
-      <Toaster />
+      <Toaster
+        position='top-center'
+        toastOptions={{
+          classNames: {
+            error: '!border-destructive !text-destructive',
+          },
+        }}
+      />
       {children}
     </ThemeProvider>
   );
