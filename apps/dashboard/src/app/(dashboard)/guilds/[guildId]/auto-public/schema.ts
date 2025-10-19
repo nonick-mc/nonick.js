06a9e3bef100f2fe6dc29e41a375871e1dc6581b
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-19 21:56:55 +0900

    feat(dashboard): 自動アナウンス公開の設定ページを実装

---
+++ apps/dashboard/src/app/(dashboard)/guilds/[guildId]/auto-public/schema.ts
@@ -0,0 +1,12 @@
+﻿import { autoPublicSetting } from '@repo/database';
+import { createInsertSchema } from 'drizzle-zod';
+import z from 'zod';
+import { snowflakeSchema } from '@/lib/zod/discord';
+import { isUniqueArray } from '@/lib/zod/utils';
+
+export const formSchema = createInsertSchema(autoPublicSetting, {
+  channels: z
+    .array(snowflakeSchema)
+    .max(20, 'チャンネルは最大20個まで設定できます。')
+    .refine(isUniqueArray, '重複した値が含まれています。'),
+}).omit({ guildId: true, createdAt: true, updatedAt: true });
