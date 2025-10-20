a40ed6244d7228042f7cac028053f307c2f83188
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-20 17:06:32 +0900

    feat(dashboard): キックログの設定ページを実装

---
+++ apps/dashboard/src/app/(dashboard)/guilds/[guildId]/event-log/kick/action.ts
@@ -0,0 +1,29 @@
+﻿'use server';
+
+import { auditLog, kickLogSetting } from '@repo/database';
+import { db } from '@/lib/drizzle';
+import { guildActionClient } from '@/lib/safe-action/clients';
+import { formSchema } from './schema';
+
+export const updateSettingAction = guildActionClient
+  .inputSchema(formSchema)
+  .action(async ({ parsedInput, bindArgsParsedInputs: [guildId], ctx: { session } }) => {
+    const currentSetting = await db.query.kickLogSetting.findFirst({
+      where: (setting, { eq }) => eq(setting.guildId, guildId),
+    });
+
+    const [newSetting] = await db
+      .insert(kickLogSetting)
+      .values({ guildId, ...parsedInput })
+      .onConflictDoUpdate({ target: kickLogSetting.guildId, set: parsedInput })
+      .returning();
+
+    db.insert(auditLog).values({
+      guildId,
+      authorId: session.user.discordUserId,
+      targetName: 'kick_log',
+      actionType: 'update_guild_setting',
+      oldValue: currentSetting,
+      newValue: newSetting,
+    });
+  });
