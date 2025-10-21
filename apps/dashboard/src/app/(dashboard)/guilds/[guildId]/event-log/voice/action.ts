c41d7332b490d089defc3c6b12c12bde23da2e1c
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-21 13:03:43 +0900

    feat(dashboard): ボイスチャットログの設定ページを実装

---
+++ apps/dashboard/src/app/(dashboard)/guilds/[guildId]/event-log/voice/action.ts
@@ -0,0 +1,32 @@
+﻿'use server';
+
+import { auditLog, voiceLogSetting } from '@repo/database';
+import { revalidatePath } from 'next/cache';
+import { db } from '@/lib/drizzle';
+import { guildActionClient } from '@/lib/safe-action/clients';
+import { formSchema } from './schema';
+
+export const updateSettingAction = guildActionClient
+  .inputSchema(formSchema)
+  .action(async ({ parsedInput, bindArgsParsedInputs: [guildId], ctx: { session } }) => {
+    const currentSetting = await db.query.voiceLogSetting.findFirst({
+      where: (setting, { eq }) => eq(setting.guildId, guildId),
+    });
+
+    const [newSetting] = await db
+      .insert(voiceLogSetting)
+      .values({ guildId, ...parsedInput })
+      .onConflictDoUpdate({ target: voiceLogSetting.guildId, set: parsedInput })
+      .returning();
+
+    db.insert(auditLog).values({
+      guildId,
+      authorId: session.user.discordUserId,
+      targetName: 'voice_log',
+      actionType: 'update_guild_setting',
+      oldValue: currentSetting,
+      newValue: newSetting,
+    });
+
+    revalidatePath('/');
+  });
---
+++ apps/dashboard/src/app/(dashboard)/guilds/[guildId]/event-log/voice/schema.ts
@@ -0,0 +1,17 @@
+﻿import { voiceLogSetting } from '@repo/database';
+import { createInsertSchema } from 'drizzle-zod';
+import { snowflakeRegex } from '@/lib/discord/constants';
+
+export const formSchema = createInsertSchema(voiceLogSetting, {
+  channel: (schema) => schema.regex(snowflakeRegex, '無効なIDです。'),
+})
+  .omit({ guildId: true, createdAt: true, updatedAt: true })
+  .superRefine((v, ctx) => {
+    if (v.enabled && !v.channel) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'チャンネルが設定されていません。',
+        path: ['channel'],
+      });
+    }
+  });
