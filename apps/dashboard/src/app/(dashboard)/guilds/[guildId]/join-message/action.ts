fd39fba0a3d13d76b3c79be30d77536f6bf59ed5
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-24 14:22:47 +0900

    feat(dashboard): 入室メッセージの設定ページを実装

---
+++ apps/dashboard/src/app/(dashboard)/guilds/[guildId]/join-message/action.ts
@@ -0,0 +1,32 @@
+﻿'use server';
+
+import { auditLog, joinMessageSetting } from '@repo/database';
+import { revalidatePath } from 'next/cache';
+import { db } from '@/lib/drizzle';
+import { guildActionClient } from '@/lib/safe-action/clients';
+import { formSchema } from './schema';
+
+export const updateSettingAction = guildActionClient
+  .inputSchema(formSchema)
+  .action(async ({ parsedInput, bindArgsParsedInputs: [guildId], ctx: { session } }) => {
+    const currentSetting = await db.query.joinMessageSetting.findFirst({
+      where: (setting, { eq }) => eq(setting.guildId, guildId),
+    });
+
+    const [newSetting] = await db
+      .insert(joinMessageSetting)
+      .values({ guildId, ...parsedInput })
+      .onConflictDoUpdate({ target: joinMessageSetting.guildId, set: parsedInput })
+      .returning();
+
+    await db.insert(auditLog).values({
+      guildId,
+      authorId: session.user.discordUserId,
+      targetName: 'join_message',
+      actionType: 'update_guild_setting',
+      oldValue: currentSetting,
+      newValue: newSetting,
+    });
+
+    revalidatePath('/');
+  });
--- apps/dashboard/src/lib/zod/discord.ts
+++ apps/dashboard/src/lib/zod/discord.ts
@@ -1,4 +1,52 @@
 ﻿import z from 'zod';
 import { snowflakeRegex } from '@/lib/discord/constants';
 
-export const snowflakeSchema = z.string().regex(snowflakeRegex, { message: '無効なIDです。' });
+// Constants
+export const snowflakeSchema = z.string().regex(snowflakeRegex, '無効なIDです。');
+
+// Embeds (フォームの機能強化に合わせてバリデーション強化が必要)
+export const embedSchema = z
+  .object({
+    title: z.string().max(256).optional(),
+    description: z.string().max(4096).optional(),
+    color: z.number().int().optional(),
+  })
+  .superRefine((v, ctx) => {
+    const embedContentsLength = [v.title?.length, v.description?.length].reduce<number>(
+      (sum, num) => sum + (num || 0),
+      0,
+    );
+
+    if (embedContentsLength > 6000) {
+      ctx.addIssue({
+        code: 'custom',
+        message: '埋め込みの内容が6000文字を超えています。',
+        path: ['title'],
+      });
+    }
+
+    if (embedContentsLength === 0) {
+      ctx.addIssue({
+        code: 'custom',
+        message: '埋め込みの内容が必要です。',
+        path: ['title'],
+      });
+    }
+  });
+
+// Message
+export const messageOptionSchema = z
+  .object({
+    content: z.string().max(2000).optional(),
+    embeds: z.array(embedSchema).max(10).optional(),
+    // 必要に応じて他のプロパティを追加
+  })
+  .superRefine((v, ctx) => {
+    if (!v.content && !v.embeds?.length) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'メッセージの内容が設定されていません。',
+        path: ['content'],
+      });
+    }
+  });
