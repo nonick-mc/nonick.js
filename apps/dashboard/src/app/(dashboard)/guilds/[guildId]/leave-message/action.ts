00d0eb8c8b4102fa0b4428832eed6d31558eb707
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-25 21:52:08 +0900

    feat(dashboard): 退室メッセージの設定ページを実装

---
+++ apps/dashboard/src/app/(dashboard)/guilds/[guildId]/leave-message/action.ts
@@ -0,0 +1,32 @@
+﻿'use server';
+
+import { auditLog, leaveMessageSetting } from '@repo/database';
+import { revalidatePath } from 'next/cache';
+import { db } from '@/lib/drizzle';
+import { guildActionClient } from '@/lib/safe-action/clients';
+import { formSchema } from './schema';
+
+export const updateSettingAction = guildActionClient
+  .inputSchema(formSchema)
+  .action(async ({ parsedInput, bindArgsParsedInputs: [guildId], ctx: { session } }) => {
+    const currentSetting = await db.query.leaveMessageSetting.findFirst({
+      where: (setting, { eq }) => eq(setting.guildId, guildId),
+    });
+
+    const [newSetting] = await db
+      .insert(leaveMessageSetting)
+      .values({ guildId, ...parsedInput })
+      .onConflictDoUpdate({ target: leaveMessageSetting.guildId, set: parsedInput })
+      .returning();
+
+    await db.insert(auditLog).values({
+      guildId,
+      authorId: session.user.discordUserId,
+      targetName: 'leave_message',
+      actionType: 'update_guild_setting',
+      oldValue: currentSetting,
+      newValue: newSetting,
+    });
+
+    revalidatePath('/');
+  });
