cd524222136740a94bb63a0c85a06785f193d260
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-27 02:50:21 +0900

    feat(database): `createdAt`および`updatedAt`のtimestampにタイムゾーンを含むように変更

--- packages/database/src/utils.ts
+++ packages/database/src/utils.ts
@@ -1,6 +1,6 @@
 ﻿import { timestamp } from 'drizzle-orm/pg-core';
 
 export const timestamps = {
-  createdAt: timestamp('create_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
+  createdAt: timestamp('create_at', { withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(() => new Date()),
 };
