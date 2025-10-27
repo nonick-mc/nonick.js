a0bc8f3329535c083c99f8d67180a7e403a27434
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-27 16:29:51 +0900

    feat(dashboard): GuildSwitcherのパフォーマンスを改善
    * `getMutualManagedGuilds()`が初期レンダリングではなく、Dropdownが開かれた際に実行されるように変更

--- apps/dashboard/src/app/(dashboard)/guilds/[guildId]/guild-switcher.tsx
+++ apps/dashboard/src/app/(dashboard)/guilds/[guildId]/guild-switcher.tsx
@@ -1,9 +1,10 @@
 ﻿import { ChevronsUpDown, PlusIcon } from 'lucide-react';
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
+import { Suspense } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import {
   DropdownMenu,
+  DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
@@ -12,7 +13,8 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { getMutualManagedGuilds } from '@/lib/discord/api';
+import { Skeleton } from '@/components/ui/skeleton';
+import { getGuild, getMutualManagedGuilds } from '@/lib/discord/api';
 import { DiscordEndPoints, inviteBotUrl } from '@/lib/discord/constants';
 
 type GuildSwitcherProps = {
@@ -20,9 +22,7 @@ type GuildSwitcherProps = {
 };
 
 export async function GuildSwitcher({ currentGuildId }: GuildSwitcherProps) {
-  const guilds = await getMutualManagedGuilds();
-  const currentGuild = guilds.find((guild) => guild.id === currentGuildId);
-  if (!currentGuild) redirect('/');
+  const currentGuild = await getGuild(currentGuildId);
 
   return (
     <SidebarMenu>
@@ -48,27 +48,22 @@ export async function GuildSwitcher({ currentGuildId }: GuildSwitcherProps) {
             align='center'
             side='bottom'
           >
-            {guilds.length && (
-              <>
-                <DropdownMenuGroup>
-                  <DropdownMenuLabel className='text-xs'>サーバー</DropdownMenuLabel>
-                  {guilds
-                    .filter((guild) => guild.id !== currentGuildId)
-                    .map((v) => (
-                      <DropdownMenuItem key={v.id} asChild>
-                        <Link href={`/guilds/${v.id}`}>
-                          <Avatar className='size-6'>
-                            <AvatarImage src={`${DiscordEndPoints.CDN}/icons/${v.id}/${v.icon}`} />
-                            <AvatarFallback>{v.name.slice(0, 2)}</AvatarFallback>
-                          </Avatar>
-                          <span>{v.name}</span>
-                        </Link>
-                      </DropdownMenuItem>
-                    ))}
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-              </>
-            )}
+            <DropdownMenuGroup>
+              <DropdownMenuLabel className='text-xs'>サーバー</DropdownMenuLabel>
+              <DropdownMenuCheckboxItem checked>
+                <Avatar className='size-6'>
+                  <AvatarImage
+                    src={`${DiscordEndPoints.CDN}/icons/${currentGuild.id}/${currentGuild.icon}`}
+                  />
+                  <AvatarFallback>{currentGuild.name.slice(0, 2)}</AvatarFallback>
+                </Avatar>
+                <span>{currentGuild.name}</span>
+              </DropdownMenuCheckboxItem>
+              <Suspense fallback={<GuildsMenuFallback />}>
+                <GuildsMenu currentGuildId={currentGuildId} />
+              </Suspense>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
             <DropdownMenuItem asChild>
               <Link href={inviteBotUrl}>
                 <PlusIcon />
@@ -79,5 +74,41 @@ export async function GuildSwitcher({ currentGuildId }: GuildSwitcherProps) {
         </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
+  );
+}
+
+function GuildsMenuFallback() {
+  return (
+    <>
+      {Array.from({ length: 2 }).map((_, index) => (
+        // biome-ignore lint/suspicious/noArrayIndexKey: Skeleton
+        <DropdownMenuCheckboxItem key={index}>
+          <Skeleton className='size-6 rounded-full' />
+          <Skeleton className='flex-1 h-4' />
+        </DropdownMenuCheckboxItem>
+      ))}
+    </>
+  );
+}
+
+async function GuildsMenu({ currentGuildId }: { currentGuildId: string }) {
+  const guilds = await getMutualManagedGuilds();
+
+  return (
+    <>
+      {guilds
+        .filter((guild) => guild.id !== currentGuildId)
+        .map((v) => (
+          <DropdownMenuItem key={v.id} asChild>
+            <Link href={`/guilds/${v.id}`}>
+              <Avatar className='size-6'>
+                <AvatarImage src={`${DiscordEndPoints.CDN}/icons/${v.id}/${v.icon}`} />
+                <AvatarFallback>{v.name.slice(0, 2)}</AvatarFallback>
+              </Avatar>
+              <span>{v.name}</span>
+            </Link>
+          </DropdownMenuItem>
+        ))}
+    </>
   );
 }
