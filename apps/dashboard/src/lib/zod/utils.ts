7b129be2a05bdc9185a2e6ae87d9ceb9197b457e
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-27 14:06:39 +0900

    chore(database): `isUniqueArray`にJSDocコメントを追加

--- apps/dashboard/src/lib/zod/utils.ts
+++ apps/dashboard/src/lib/zod/utils.ts
@@ -1,1 +1,2 @@
-﻿export const isUniqueArray = <T>(array: T[]): boolean => array.length === new Set(array).size;
+﻿/** 配列内の要素が重複していないかを確認する */
+export const isUniqueArray = <T>(array: T[]): boolean => array.length === new Set(array).size;
