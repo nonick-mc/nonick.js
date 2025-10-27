ab2999f8de0027ee6810223511bd1ca9ab994c90
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-27 18:52:13 +0900

    feat(dashboard): サーバーIDコピーボタンをUXを改善

--- apps/dashboard/src/app/(dashboard)/guilds/[guildId]/(dashboard)/guild-info-card-action.tsx
+++ apps/dashboard/src/app/(dashboard)/guilds/[guildId]/(dashboard)/guild-info-card-action.tsx
@@ -1,39 +1,30 @@
 ﻿'use client';
 
-import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
-import { CopyIcon, EllipsisIcon } from 'lucide-react';
+import { CheckIcon, CopyIcon } from 'lucide-react';
 import { useParams } from 'next/navigation';
+import { useState } from 'react';
 import { toast } from 'sonner';
 import { useCopyToClipboard } from 'usehooks-ts';
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 
 export function GuildInfoCardAction() {
   const { guildId } = useParams<Awaited<PageProps<'/guilds/[guildId]'>['params']>>();
   const [_, copy] = useCopyToClipboard();
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const handleCopy = async () => {
+    setIsDisabled(true);
     await copy(guildId);
     toast.success('サーバーIDをコピーしました');
+    setTimeout(() => {
+      setIsDisabled(false);
+    }, 3000);
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='ghost' size='icon'>
-          <EllipsisIcon />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={handleCopy}>
-          <CopyIcon />
-          サーバーIDをコピー
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant='outline' onClick={handleCopy} disabled={isDisabled}>
+      {isDisabled ? <CheckIcon /> : <CopyIcon />}
+      <span className='hidden sm:block'>サーバーIDをコピー</span>
+    </Button>
   );
 }
