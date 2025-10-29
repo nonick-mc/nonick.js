2a2cf6628f658566deff288ee93cee2fa7551b3c
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-10-29 14:16:10 +0900

    feat(bot): 新しい自動スレッド作成の動作を実装し、既存の動作を削除

---
+++ apps/bot/src/events/message/autoCreateThread.ts
@@ -0,0 +1,31 @@
+﻿import { Events } from 'discord.js';
+import { autoCreateThreadHolder } from '@/constants/holder';
+import { db } from '@/modules/drizzle';
+import { DiscordEventBuilder } from '@/modules/events';
+
+export default new DiscordEventBuilder({
+  type: Events.MessageCreate,
+  async execute(message) {
+    if (!message.inGuild()) return;
+
+    const rule = await db.query.autoCreateThreadRule.findFirst({
+      where: (rule, { eq, and }) =>
+        and(eq(rule.guildId, message.guildId), eq(rule.channelId, message.channelId)),
+    });
+
+    if (!rule?.enabled) return;
+    if (rule.ignoreRoles.some((role) => message.member?.roles.cache.has(role))) return;
+
+    const threadName = autoCreateThreadHolder.parse(rule.threadName, {
+      member: message.member,
+      user: message.author,
+      createdAt: message.createdAt,
+    });
+
+    message.startThread({
+      name: threadName.length > 100 ? `${threadName.slice(0, 97)}...` : threadName,
+      autoArchiveDuration: rule.autoArchiveDuration,
+      reason: '自動スレッド作成',
+    });
+  },
+});
