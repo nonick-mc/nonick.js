ee76064692cfe80911b73dbe840c714f343d470c
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-11-10 10:16:33 +0900

    fix(dashboard): トーストが表示されない問題を修正

--- apps/dashboard/src/app/provider.tsx
+++ apps/dashboard/src/app/provider.tsx
@@ -1,9 +1,9 @@
 ﻿'use client';
 
-import { Toaster } from '@repo/ui/components/sonner';
 import { ThemeProvider } from 'next-themes';
 import NextTopLoader from 'nextjs-toploader';
 import type { PropsWithChildren } from 'react';
+import { Toaster } from '@/components/sonner';
 
 export function Provider({ children }: PropsWithChildren) {
   return (
