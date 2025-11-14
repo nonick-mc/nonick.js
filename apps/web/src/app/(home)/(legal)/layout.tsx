158f9094f8f5e314caedc11ec38e47c40574d021
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-11-14 10:10:25 +0900

    fix(web): `@tailwindcss/typography`を削除し、ドキュメント内のレイアウトが崩れていた問題を修正

--- apps/web/src/app/(home)/(legal)/layout.tsx
+++ apps/web/src/app/(home)/(legal)/layout.tsx
@@ -1,3 +1,3 @@
 ﻿export default function Layout({ children }: LayoutProps<'/'>) {
-  return <article className='prose dark:prose-invert max-w-none py-8'>{children}</article>;
+  return <article className='prose py-8'>{children}</article>;
 }
