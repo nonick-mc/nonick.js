f26c33f7e2621a1a2e62c70a08ebf3d32b6efbda
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-11-14 09:57:29 +0900

    feat(web): 利用規約・プレイバシーポリシーを追加

--- apps/web/src/app/(home)/layout.tsx
+++ apps/web/src/app/(home)/layout.tsx
@@ -2,5 +2,14 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+  return (
+    <div className='container max-w-6xl'>
+      <HomeLayout
+        {...baseOptions()}
+        style={{ '--spacing-fd-container': 'var(--container-6xl)' } as object}
+      >
+        {children}
+      </HomeLayout>
+    </div>
+  );
 }
