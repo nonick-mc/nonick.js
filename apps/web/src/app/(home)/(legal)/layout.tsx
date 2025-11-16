6b8050170dc51783aeee2d6eb1760dbf3d2bfa5d
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-11-16 23:15:42 +0900

    style(web): 利用規約 ・プライバシーポリシーのコンテンツ最大幅を変更

--- apps/web/src/app/(home)/(legal)/layout.tsx
+++ apps/web/src/app/(home)/(legal)/layout.tsx
@@ -1,3 +1,3 @@
 ﻿export default function Layout({ children }: LayoutProps<'/'>) {
-  return <article className='prose py-8'>{children}</article>;
+  return <article className='prose py-8 max-w-3xl mx-auto'>{children}</article>;
 }
