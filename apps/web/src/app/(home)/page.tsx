62713da25305649558abcfb2a9c69ddbc16ddb86
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-11-16 23:14:28 +0900

    feat(web): ホームページのheroセクションを実装

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -1,16 +1,12 @@
 import Link from 'next/link';
+import { HeroSection } from './_section/hero';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
-      <p>
-        You can open{' '}
-        <Link href="/docs" className="font-medium underline">
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </div>
+    <main className='overflow-hidden'>
+      <section className='relative'>
+        <HeroSection />
+      </section>
+    </main>
   );
 }
---
+++ apps/web/src/lib/constants.ts
@@ -0,0 +1,4 @@
+﻿export enum Links {
+  Dashboard = 'https://docs.nonick-js.com',
+  SupportServer = 'https://discord.gg/q4FcBm2P42',
+}
