22492edf52c253ed099418ff7c3ad526f631d179
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-11-17 11:51:23 +0900

    feat(web): ホームページのGalleryセクションを実装

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -1,10 +1,14 @@
+import { Separator } from '@repo/ui/components/separator';
 import { FaqSection } from './_section/faq';
+import { GallerySection } from './_section/gallery';
 import { HeroSection } from './_section/hero';
 
 export default function HomePage() {
   return (
     <main className='overflow-hidden'>
         <HeroSection />
+      <Separator />
+      <GallerySection />
       <Separator />
         <FaqSection />
       
