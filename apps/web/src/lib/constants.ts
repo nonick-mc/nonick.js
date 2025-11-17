3a723e00e876a5eea8b9fb3efc4324c71155ad86
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-11-17 16:14:09 +0900

    hotfix(web): ホームページのダッシュボードURLを修正

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -1,5 +1,5 @@
 ﻿export enum Links {
-  Dashboard = 'https://nonick-js.com/docs',
+  Dashboard = 'https://dashboard.nonick-js.com',
   SupportServer = 'https://discord.gg/q4FcBm2P42',
   Roadmap = 'https://github.com/users/nonick-mc/projects/2',
 }
