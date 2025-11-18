13aac3951c8b0a79280a7bfd975191d14697cd66
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-11-18 14:17:11 +0900

    feat(bot): sunarのセットアップ

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,19 +1,7 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 
-import path from 'node:path';
-import { Cron } from '@/modules/cron';
-import { DiscordEvents } from '@/modules/events';
-import { DiscordInteractions, ErrorCodes, InteractionsError } from '@akki256/discord-interaction';
-import {
-  ActivityType,
-  AllowedMentionsTypes,
-  Client,
-  Events,
-  GatewayIntentBits,
-  Partials,
-  version,
-} from 'discord.js';
+import { AllowedMentionsTypes, GatewayIntentBits, Partials } from 'discord.js';
+import { Client, dirname, load } from 'sunar';
 
 export const client = new Client({
   intents: [
@@ -31,54 +19,14 @@ export const client = new Client({
   },
 });
 
-const interactions = new DiscordInteractions(client);
-interactions.loadRegistries(path.resolve(__dirname, './interactions'));
-
-const events = new DiscordEvents(client);
-events.register(path.resolve(__dirname, './events'));
-
-Cron.registerFiles(path.resolve(__dirname, './cron'));
-
-client.once(Events.ClientReady, async () => {
-  console.log('[INFO] BOT ready!');
-  console.table({
-    'Bot User': client.user?.tag,
-    Guilds: `${client.guilds.cache.size} Servers`,
-    Watching: `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} Members`,
-    'Discord.js': `v${version}`,
-    'Node.js': process.version,
-    Platform: `${process.platform} | ${process.arch}`,
-    Memory: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(
-      2,
-    )}MB | ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}MB`,
-  });
-
-  client.user?.setActivity({
-    name: `/help | ${(await client.application?.fetch())?.approximateGuildCount} servers`,
-    type: ActivityType.Competing,
-  });
-
-  interactions.registerCommands({
-    guildId: process.env.GUILD_ID ?? undefined,
-    syncWithCommand: true,
-  });
-});
-
-client.on(Events.InteractionCreate, (interaction) => {
-  if (!interaction.isRepliable()) return;
-
-  interactions.run(interaction).catch((err) => {
-    if (err instanceof InteractionsError && err.code === ErrorCodes.CommandHasCoolTime)
-      return interaction.reply({
-        content: '`⌛` コマンドはクールダウン中です',
-        ephemeral: true,
-      });
-    console.error(err);
-  });
-});
+const start = async () => {
+  const base = dirname(import.meta.url);
+  await load(`${base.split('/').at(-1)}/{commands,signals}/**/*.{js,ts}`);
+  client.login();
+};
 
 process.on('uncaughtException', (err) => {
   console.error(err);
 });
 
-client.login();
+start();
---
+++ apps/bot/src/signals/ready.ts
@@ -0,0 +1,29 @@
+﻿import { ActivityType, version } from 'discord.js';
+import { execute, Signal, Signals } from 'sunar';
+import { registerCommands, registerGuildCommands } from 'sunar/registry';
+
+export const signal = new Signal(Signals.ClientReady, { once: true });
+
+execute(signal, async (client) => {
+  if (process.env.DEV_GUILD_ID) {
+    console.log('[INFO] Registering guild commands for development guild...');
+    await registerGuildCommands(client.application, [process.env.DEV_GUILD_ID]);
+  } else {
+    console.log('[INFO] Registering global commands...');
+    await registerCommands(client.application);
+  }
+
+  client.user?.setActivity({
+    name: `/help | ${(await client.application?.fetch())?.approximateGuildCount} servers`,
+    type: ActivityType.Competing,
+  });
+
+  console.log('[INFO] BOT ready!');
+  console.table({
+    'Bot User': client.user?.tag,
+    Guilds: `${client.guilds.cache.size} Servers`,
+    Watching: `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} Members`,
+    'Discord.js': `v${version}`,
+    'Node.js': process.version,
+  });
+});
