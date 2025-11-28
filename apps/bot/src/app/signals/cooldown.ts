93eb897c48fc3ebcfb1e78f3ea8e51081beb112e
nonick-mc <87268971+nonick-mc@users.noreply.github.com>
2025-11-28 14:22:46 +0900

    feat(bot): コマンドクールダウン時のメッセージを実装

---
+++ apps/bot/src/app/signals/cooldown.ts
@@ -0,0 +1,12 @@
+import { inlineCode, MessageFlags } from 'discord.js';
+import { execute, Signal, Signals } from 'sunar';
+import { getBotEmoji, warning } from '@/constants/emojis.js';
+
+export const signal = new Signal(Signals.Cooldown);
+
+execute(signal, (interaction, context) => {
+  interaction.reply({
+    content: `${getBotEmoji(warning.circleAlert)} コマンドはクールダウン中です。${inlineCode(Math.floor(context.remaining / 1000).toString())}秒後に再試行してください。`,
+    flags: [MessageFlags.Ephemeral],
+  });
+});
--- apps/bot/src/constants/emojis.ts
+++ apps/bot/src/constants/emojis.ts
@@ -4,13 +4,18 @@ export const success = {
   circleCheck: 'success_circle_check',
 } satisfies Record<string, `success_${string}`>;
 
+export const warning = {
+  circleAlert: 'warning_circle_alert',
+} satisfies Record<string, `warning_${string}`>;
+
 export const danger = {
   circleX: 'danger_circle_x',
 } satisfies Record<string, `danger_${string}`>;
 
 type successEmojiName = (typeof success)[keyof typeof success];
+type warningEmojiName = (typeof warning)[keyof typeof warning];
 type dangerEmojiName = (typeof danger)[keyof typeof danger];
-type EmojiName = successEmojiName | dangerEmojiName;
+type EmojiName = successEmojiName | warningEmojiName | dangerEmojiName;
 
 export function getBotEmoji(emojiName: EmojiName) {
   return client.application?.emojis.cache.find((emoji) => emoji.name === emojiName) ?? '❌';
