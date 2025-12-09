26099f0d0ddc55b26038d27723d5b6eca88e0f33
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2025-12-09 12:30:38 +0900

    chore(bot): クールダウンシステムの関数をasync関数に変更

--- apps/bot/src/app/signals/cooldown.ts
+++ apps/bot/src/app/signals/cooldown.ts
@@ -4,7 +4,7 @@ import { getBotEmoji, warning } from '@/constants/emojis.js';
 
 export const signal = new Signal(Signals.Cooldown);
 
-execute(signal, (interaction, context) => {
+execute(signal, async (interaction, context) => {
   interaction.reply({
     content: `${getBotEmoji(warning.circleAlert)} コマンドはクールダウン中です。${inlineCode(Math.floor(context.remaining / 1000).toString())}秒後に再試行してください。`,
     flags: [MessageFlags.Ephemeral],
