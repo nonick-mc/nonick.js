0888681a6e18348dff5276444c0f88c121488351
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2026-02-22 15:28:23 +0900

    feat(bot): messageCreate時のGuildテーブルの更新処理を削除 (#97)

--- apps/bot/src/events/bot/manageGuildModel.ts
+++ apps/bot/src/events/bot/manageGuildModel.ts
@@ -1,47 +1,21 @@
-import { db } from '@/modules/drizzle';
-import { DiscordEventBuilder } from '@/modules/events';
 import { guild } from '@repo/database';
 import { Events } from 'discord.js';
 import { eq } from 'drizzle-orm';
-
-const GuildCache = new Set<string>();
+import { db } from '@/modules/drizzle';
+import { DiscordEventBuilder } from '@/modules/events';
 
 const onGuildCreate = new DiscordEventBuilder({
   type: Events.GuildCreate,
-  async execute(guild) {
-    createGuild(guild.id);
-  },
-});
-
-const onMessageCreate = new DiscordEventBuilder({
-  type: Events.MessageCreate,
-  async execute(message) {
-    if (!message.inGuild()) return;
-    createGuild(message.guild.id);
+  async execute(apiGuild) {
+    await db.insert(guild).values({ id: apiGuild.id }).onConflictDoNothing();
   },
 });
 
 const onGuildDelete = new DiscordEventBuilder({
   type: Events.GuildDelete,
   async execute(apiGuild) {
-    db.delete(guild)
-      .where(eq(guild.id, apiGuild.id))
-      .then(() => {
-        GuildCache.delete(apiGuild.id);
-      });
+    await db.delete(guild).where(eq(guild.id, apiGuild.id));
   },
 });
 
-async function createGuild(guildId: string) {
-  const dbGuild = await db.query.guild.findFirst({
-    where: (setting, { eq }) => eq(setting.id, guildId),
-  });
-
-  if (GuildCache.has(guildId) || dbGuild) return;
-  await db
-    .insert(guild)
-    .values({ id: guildId })
-    .then(() => GuildCache.add(guildId));
-}
-
-export default [onGuildCreate, onMessageCreate, onGuildDelete];
+export default [onGuildCreate, onGuildDelete];
