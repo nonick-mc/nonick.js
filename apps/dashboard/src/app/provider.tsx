0a25a4ced45f943ddf6ea60ede19df930661927a
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2026-05-08 22:55:33 +0900

    feat(dashboard): サーバー検索欄の内部実装を変更

--- apps/dashboard/src/app/provider.tsx
+++ apps/dashboard/src/app/provider.tsx
@@ -3,22 +3,25 @@
 import { Toaster } from '@repo/ui/components/sonner';
 import { ThemeProvider } from 'next-themes';
 import NextTopLoader from 'nextjs-toploader';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import type { PropsWithChildren } from 'react';
 
 export function Provider({ children }: PropsWithChildren) {
   return (
-    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-      <NextTopLoader color='#2563eb' showSpinner={false} />
-      <Toaster
-        position='top-center'
-        toastOptions={{
-          classNames: {
-            error: '!border-destructive !text-destructive',
-            success: '!border-green-600 !text-green-600',
-          },
-        }}
-      />
-      {children}
-    </ThemeProvider>
+    <NuqsAdapter>
+      <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+        <NextTopLoader color='#2563eb' showSpinner={false} />
+        <Toaster
+          position='top-center'
+          toastOptions={{
+            classNames: {
+              error: '!border-destructive !text-destructive',
+              success: '!border-green-600 !text-green-600',
+            },
+          }}
+        />
+        {children}
+      </ThemeProvider>
+    </NuqsAdapter>
   );
 }
